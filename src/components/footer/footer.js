9856e4bf81b466d50cec1df7f17bcece73c8e75c
LENA EIVY <applena@gmail.com>
2019-04-01 13:29:43 -0700

    initial commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.scss';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+        <p>@copyright Lena Eivy 2019</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
