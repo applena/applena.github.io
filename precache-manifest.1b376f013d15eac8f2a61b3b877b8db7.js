f28ba54ed9c9d45ed00ab61c2f3c352f33de6e54
LENA EIVY <applena@gmail.com>
2019-04-06 09:19:44 -0700

    Updates

---
+++ precache-manifest.1b376f013d15eac8f2a61b3b877b8db7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed117a8c40123739ddb6",
+    "url": "/static/css/main.26dd3e06.chunk.css"
+  },
+  {
+    "revision": "ed117a8c40123739ddb6",
+    "url": "/static/js/main.09baf5ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "814c662da4885b85e08d",
+    "url": "/static/js/2.c5283627.chunk.js"
+  },
+  {
+    "revision": "eb17b46f513d1c99597ccc47d3ac257a",
+    "url": "/static/media/bazaar.eb17b46f.png"
+  },
+  {
+    "revision": "b21102fcdb116466966ecbdafd5b09da",
+    "url": "/static/media/pic02.b21102fc.png"
+  },
+  {
+    "revision": "64a371bc1d39df55baa2fea818360807",
+    "url": "/static/media/pic04.64a371bc.png"
+  },
+  {
+    "revision": "032af08acaf1e42cc27c59b97409d9ef",
+    "url": "/static/media/pic06.032af08a.png"
+  },
+  {
+    "revision": "5d8e0d36b2a200bfc48fd33bc53414c6",
+    "url": "/static/media/pic07.5d8e0d36.png"
+  },
+  {
+    "revision": "157c8aedc0d3cfa66e7ab0e3463e82cb",
+    "url": "/static/media/pic9.157c8aed.png"
+  },
+  {
+    "revision": "4f1a1c9209d676e520b34d99cfaf391c",
+    "url": "/static/media/lenaBW.4f1a1c92.jpg"
+  },
+  {
+    "revision": "0ff2d97328d8f9938b2582af9a47defb",
+    "url": "/static/media/banner.0ff2d973.jpg"
+  },
+  {
+    "revision": "a2c3a8ef7aee40bc31824ac987594ee5",
+    "url": "/index.html"
+  }
+];
