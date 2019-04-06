073e6c7e6a91aa0e91c4c48f44fb5b6fca2de8d0
LENA EIVY <applena@gmail.com>
2019-04-06 08:41:52 -0700

    updated bio

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,7 +9,7 @@ class About extends React.Component{
         <h2>About Me</h2>
         <img src={lenaBW} />
         <p>
-          Creative, client-centered Full-Stack Software Engineer who successfully grew a thriving business with year over year growth for 8 years. Flourishes under the pressure of deadlines while implementing the newest design theories and best business practices to ensure client satisfaction. Expert in the tools of the industry including JavaScript, React, Redux, Node.js, SASS, CSS, HTML, Adobe Photoshop, Adobe Lightroom, and Adobe Illustrator.
+          Creative, client-centered Full-Stack Software Engineer who successfully grew a thriving business with year over year growth for 8 years. Flourishes under the pressure of deadlines while implementing the newest design theories and best business practices to ensure client satisfaction. Expert in the tools of the industry including JavaScript, React, Redux, Node.js, SASS, CSS, HTML, SQL, Postgress, Mongo, git, Adobe Photoshop, Adobe Lightroom, and Adobe Illustrator.
         </p>
         <p>
           Specialties: JavaScript, React, Redux, Context, Node.js, SASS, CSS, github, Adobe Creative Suite, Working as part of a team, Inspiring confidence, Managing Projects.
