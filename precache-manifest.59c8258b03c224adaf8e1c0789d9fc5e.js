426adaaeda1255b9cf8bcc67b1da6eb6a51d9f04
LENA EIVY <applena@gmail.com>
2019-11-27 13:14:08 -0800

    Updates

--- precache-manifest.59c8258b03c224adaf8e1c0789d9fc5e.js
+++ precache-manifest.59c8258b03c224adaf8e1c0789d9fc5e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ed117a8c40123739ddb6",
-    "url": "/static/css/main.26dd3e06.chunk.css"
+    "revision": "67bb1c791bb08b2e5d3bfd5a6fe91b91",
+    "url": "/static/media/knowVote.67bb1c79.png"
   },
   {
-    "revision": "ed117a8c40123739ddb6",
-    "url": "/static/js/main.09baf5ca.chunk.js"
+    "revision": "422e86d994f0b32af53f",
+    "url": "/static/css/main.9f1ac72f.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -28,8 +28,8 @@ self.__precacheManifest = [
     "url": "/static/media/pic04.64a371bc.png"
   },
   {
-    "revision": "032af08acaf1e42cc27c59b97409d9ef",
-    "url": "/static/media/pic06.032af08a.png"
+    "revision": "422e86d994f0b32af53f",
+    "url": "/static/js/main.1a67e9a1.chunk.js"
   },
   {
     "revision": "5d8e0d36b2a200bfc48fd33bc53414c6",
@@ -44,11 +44,15 @@ self.__precacheManifest = [
     "url": "/static/media/lenaBW.4f1a1c92.jpg"
   },
   {
+    "revision": "032af08acaf1e42cc27c59b97409d9ef",
+    "url": "/static/media/pic06.032af08a.png"
+  },
+  {
     "revision": "0ff2d97328d8f9938b2582af9a47defb",
     "url": "/static/media/banner.0ff2d973.jpg"
   },
   {
-    "revision": "a2c3a8ef7aee40bc31824ac987594ee5",
+    "revision": "45e4a86a18baeb063a80359b7e3fd89e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b376f013d15eac8f2a61b3b877b8db7.js"
+  "/precache-manifest.59c8258b03c224adaf8e1c0789d9fc5e.js"
 );
 
 workbox.clientsClaim();
